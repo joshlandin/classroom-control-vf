5e8145fa42fb459b5d043ffee657134f65fdc5a9
Josh Landin <joshlandin@users.noreply.github.com>
2016-05-24 17:45:33 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -13,9 +13,27 @@ class nginx {
     source => 'puppet:///modules/nginx/nginx.conf',
   }
   
+  file { '/etc/nginx/conf.d/default.conf':
+    ensure => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nginx'],
+    source => 'puppet:///modules/nginx/default.conf',
+  }
+  
+  file { '/var/www/index.html':
+    ensure => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package['nginx'],
+    source => 'puppet:///modules/nginx/index.html',
+  }
+  
   service { 'nginx':
     enable  => true,
-    require => File['/etc/nginx/nginx.conf'],
+    require => File['/etc/nginx/nginx.conf','/etc/nginx/conf.d/default.conf'],
   }
   
 }
