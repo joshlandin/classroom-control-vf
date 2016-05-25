708751994add2b372f9bc9d29eb29750c2fef60d
Josh Landin <joshlandin@users.noreply.github.com>
2016-05-25 14:01:26 -0400

    Create vhost.pp

---
+++ site/nginx/manifests/vhost.pp
@@ -0,0 +1,23 @@
+define nginx::vhost (
+  $port = 80,
+  $servername = $title,
+  $docroot = "${nginx::docroot}/vhosts/${title}",
+) {
+
+  file { $docroot:
+    ensure => directory,
+  }
+
+  file { "$docroot/index.html":
+    ensure  => file,
+    content => template('nginx/index.html.erb'),
+  }
+
+  file { "nginx-vhost-${title}":
+    ensure  => file,
+    path    => "${nginx::blockdir}/${title}.conf",
+    content => template('nginx/index.html.erb'),
+    notify  => Service['nginx'],
+  }
+
+}
